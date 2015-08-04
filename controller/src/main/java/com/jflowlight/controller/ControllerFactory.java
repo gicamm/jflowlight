98eac2c2ca2b7b48e01287f29aa7478f76821610
Giovanni Cammarata <cammarata.giovanni@gmail.com>
2015-08-04 10:32:13 +0200

    Added some docs

--- controller/src/main/java/com/jflowlight/controller/ControllerFactory.java
+++ controller/src/main/java/com/jflowlight/controller/ControllerFactory.java
@@ -12,18 +12,89 @@ import java.text.MessageFormat;
  */
 public class ControllerFactory {
 
+
+        private String controllerType;
+        private String version;
+        private String address;
+        private Integer port;
+        private String user;
+        private String pwd;
+
         /**
          *
-         * @param controllerType the controller type (e.g. opendaylight)
-         * @param version the version. Currently not supported
-         * @param address
-         * @param port
-         * @param user
-         * @param pwd
          * @return
          * @throws JolException
          */
-        public static Controller get(final String controllerType, final String version, final String address, final Integer port, final String user, final String pwd) throws JolException{
+        public static ControllerFactory builder() throws JolException{
+                return new ControllerFactory();
+        }
+
+        /**
+         *
+         * @param controllerType
+         * @return
+         */
+        public ControllerFactory setControllerType(final String controllerType) {
+                this.controllerType = controllerType;
+                return this;
+        }
+
+        /**
+         *
+         * @param version
+         * @return
+         */
+        public ControllerFactory setVersion(final String version) {
+                this.version = version;
+                return this;
+        }
+
+        /**
+         *
+         * @param address
+         * @return
+         */
+        public ControllerFactory setAddress(final String address) {
+                this.address = address;
+                return this;
+        }
+
+        /**
+         *
+         * @param port
+         * @return
+         */
+        public ControllerFactory setPort(final Integer port) {
+                this.port = port;
+                return this;
+        }
+
+        /**
+         *
+         * @param user
+         * @return
+         */
+        public ControllerFactory setUser(final String user) {
+                this.user = user;
+                return this;
+        }
+
+        /**
+         *
+         * @param pwd
+         * @return
+         */
+        public ControllerFactory setPwd(final String pwd) {
+                this.pwd = pwd;
+                return this;
+        }
+
+        /**
+         *
+         * @return
+         * @throws JolException
+         */
+        public Controller build() throws JolException{
                 if(StringUtils.isBlank(controllerType))
                         throw new JolException("Type must not be blank");
 
--- controller/src/test/java/com/jflowlight/controller/ControllerFactoryTest.java
+++ controller/src/test/java/com/jflowlight/controller/ControllerFactoryTest.java
@@ -3,8 +3,6 @@ package com.jflowlight.controller;
 import org.jflowlight.controller.Controller;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * @author Giovanni Cammarata <cammarata.giovanni@gmail.com>
  */
@@ -12,7 +10,13 @@ public class ControllerFactoryTest {
 
     @Test
     public void testGet() throws Exception {
-        final Controller controller = ControllerFactory.get("opendaylight",null, "address", 8181, "user", "strong_pwd");
+        final Controller controller = ControllerFactory.builder().
+                setControllerType("opendaylight").
+                setAddress("192.168.1.55").
+                setPort(8181).
+                setUser("usr").
+                setPwd("strong_pwd").
+                build();
     }
 
 }
